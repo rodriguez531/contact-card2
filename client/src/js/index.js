a7b09530804e0974a333ed3d8242932db9ec77c2
rodriguez531 <alexr5408@gmail.com>
2023-11-27 15:33:31 -0500

    added css module and html and css plugins

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,8 +1,11 @@
 import "./form";
 import "./submit";
+
 import "../css/index.css";
-import { Tooltip, Toast, Popover } from 'bootstrap';
+
+import { Tooltip, Toast, Popover } from "bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
